c2943a831e8a0f9392546789f8923f477cef77c1
Thais Bobato <105284617+thaisbobato@users.noreply.github.com>
2023-10-10 09:21:19 -0300

    ok

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function tocaSomPOm(){
+function tocaSomPom(){
     document .querySelector('#som_tecla_pom');
 }
-document .querySelector 
+document .querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){
+    document .querySelector('#som_tecla_clap') .play();
+     }
+     document .querySelector('.tecla_clap') .onclick = tocaSomClap;
