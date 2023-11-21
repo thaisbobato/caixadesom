66cea3e03f9e95c397a6a6184885fab5b5d0a13c
Thais Bobato <105284617+thaisbobato@users.noreply.github.com>
2023-11-21 09:20:39 -0300

    ok

--- main.js
+++ main.js
@@ -18,9 +18,13 @@ for(let contador = 0;contador < listaDeTeclas.lenght;contador = contador++){
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
-    tecla.onkeydown = function() {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(evento) {
+        if(EventCounts.code != 'Tab') {
+            tecla.classList.add('ativa');
+        }
+       
     }
+
     tecla.onkeyup = function() {
         tecla.classList.remove('ativa');
     }
